e01efbe1a4caee493f32c50b73489a29f8df37a9
Adailson Aguiar <adailsonacj@live.com>
2019-10-14 20:52:09 -0300

    listando os spots cadastrados

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+import './styles.css';
 
 export default function Dashboard() {
-  return <div>dashboard</div>;
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    const loadSpots = async () => {
+      const user_id = localStorage.getItem('user');
+      const response = await api.get('/dashboard', { headers: { user_id } });
+      setSpots(response.data);
+    };
+    loadSpots();
+  }, []);
+
+  return (
+    <>
+      <ul className='spot-list'>
+        {spots.map(spot => (
+          <li key={spot['_id']}>
+            <img src={spot['thumbnail_url']} />
+
+            <strong>{spot['company']}</strong>
+            <span>{spot['price'] ? `R$${spot['price']}/ dia` : 'GRATUITO'}</span>
+          </li>
+        ))}
+      </ul>
+      <Link to='/new'>Cadastrar novo Spot</Link>
+    </>
+  );
 }
